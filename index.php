a2b0a2bd311b5f7c53f1dae3bde04581d3258240
GaSeS <very.nice@ukr.net>
2018-07-25 09:08:46 +0300

    РОбота з буфуром виводу. Завдання 2

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
 
 ob_start();
-
-{
-    echo 'hello';
-    $a = ob_get_contents();
-    ob_clean();
-    echo 'world';
-    $b = ob_get_contents();
-}
-
-ob_end_clean();
-
-echo $a.' '.$b;
+    ob_start();
+        ob_start();
+            echo ob_get_level();  // 4
+        ob_end_flush();
+        echo ob_get_level();  //3
+    ob_end_flush();
+    echo ob_get_level(); //2
+ob_end_flush();
+echo ob_get_level();  //1
